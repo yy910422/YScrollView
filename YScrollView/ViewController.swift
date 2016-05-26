49651ec83a7b7849f6620d726e7bc7e0979b0232
yy910422 <yy9103@163.com>
2016-05-26 13:30:38 +0800

    Initial Commit

---
+++ YScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YScrollView
+//
+//  Created by apple on 16/5/26.
+//  Copyright © 2016年 bg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
